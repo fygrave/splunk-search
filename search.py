91e7cea60d198d3451fb7c113468c542e68ced7e
Kyle Maxwell <kylem@xwell.org>
2014-02-04 20:38:09 -0600

    Basic CybOX parsing

--- search.py
+++ search.py
@@ -1,15 +1,27 @@
 import argparse
+import json
 import sys
+
+import cybox.bindings.cybox_core as cybox_core_binding
+from cybox.core import Observables
 
 import ConfigParser
 
 
 def read_cybox(input_file, isJson):
-    pass
+    if not isJson:
+        # Based on https://github.com/CybOXProject/python-cybox/blob/master/examples/parse_xml.py
+        observables_obj = cybox_core_binding.parse(input_file)
+        observables = Observables.from_obj(observables_obj)
+        return observables.to_dict()
+    else:
+        with open(input_file, 'rb') as f:
+            return json.load(input_file)
 
 
 def read_openioc(input_file):
-    pass
+    sys.stderr.write('Not yet implemented')
+    return False
 
 
 def search_splunk(data):
@@ -21,7 +33,8 @@ def main():
 
     parser = argparse.ArgumentParser(description="Search Splunk for indicators in CybOX or OpenIOC files")
     parser.addargument('input_file', help="Input file")
-    parser.addargument('-t', '--filetype', help="Type of file (optional). If specified, must be one of: cybox, cybox-json, openioc")
+    parser.addargument('-t', '--filetype', choices=['cybox', 'cybox-json', 'openioc'],
+                       help="Type of file (optional). If specified, must be one of: cybox, cybox-json, openioc")
 
     args = parser.parse_args()
     ioc_data = {}
@@ -34,10 +47,11 @@ def main():
         ioc_data = read_openioc(args.input_file)
     else:
         sys.stderr.write("File type not properly specified, see --help for more.")
-        return False
+        return
 
-    search_splunk(ioc_data)
+    if ioc_data:
+        search_splunk(ioc_data)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
