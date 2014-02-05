82b5055d392c97e652039df9aba85ea9a0010b90
Kyle Maxwell <kylem@xwell.org>
2014-02-04 22:32:06 -0600

    Script for testing connection to Splunk instance

--- test_connection.py
+++ test_connection.py
@@ -2,4 +2,13 @@ import splunklib.client as splunk_client
 import ConfigParser
 
 config = ConfigParser.ConfigParser()
-config.r
+config.read('.splunkrc')
+
+host = config.get('Splunk', 'host')
+port = config.getint('Splunk', 'port')
+username = config.get('Splunk', 'username')
+password = config.get('Splunk', 'password')
+
+splunk_connection = splunk_client.connect(host=host, port=port, username=username, password=password)
+
+print splunk_connection.splunk_version
